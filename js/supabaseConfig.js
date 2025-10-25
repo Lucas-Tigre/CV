6787415525a19fe3727e00d9453cd18b1205f6b3
Lucas-Guercheski <168201998+Lucas-Tigre@users.noreply.github.com>
2025-10-24 23:42:04 -0300

    sdfghjkl

--- js/supabaseConfig.js
+++ js/supabaseConfig.js
@@ -15,5 +15,5 @@
 // Adicione-o ao seu .gitignore se estiver trabalhando em um repositório público.
 // -----------------------------------------------------------------------------
 
-export const SUPABASE_URL = "SUA_URL_DO_SUPABASE_AQUI";
-export const SUPABASE_ANON_KEY = "SUA_CHAVE_ANON_AQUI";
+export const SUPABASE_URL = "https://koliolijmlzifxulyejz.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvbGlvbGlqbWx6aWZ4dWx5ZWp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyOTEwNDAsImV4cCI6MjA3Mzg2NzA0MH0.s44jYbJDjZqjKwPBCdsJ5WZhmXI9xVggxMgH7IKFGYE";
